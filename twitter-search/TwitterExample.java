a7d871f307a6ed5563e2c490b57553fd2922ff58
Joseph Tinglof <jbtinglof@gmail.com>
2018-11-14 18:35:08 -0800

    Add files via upload

---
+++ twitter-search/TwitterExample.java
@@ -0,0 +1,49 @@
+/**
+ *  Program #8
+ *  This program displays a desired number of the most recent Tweets that use a 
+ *  user specified word or phrase. 
+ *  CS108-2
+ *  12-13-16
+ *  @author  Joseph Tinglof
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import realtimeweb.twittersearch.TwitterSearch;
+import realtimeweb.twittersearch.domain.Tweet;
+
+public class TwitterExample {
+    public static void main(String[] args) {
+    	
+    	//Initialize the varables.
+    	int numTweets;
+    	String searchTerm;
+    	Scanner radar = new Scanner(System.in);
+    	
+    	try{
+    		//Asks for and stores desired number of Tweets and topic to search for.
+    		System.out.println("Program 8, Joseph Tinglof, masc0752");
+    		System.out.println("Please enter the word or phrase you would like to search for:");
+    		searchTerm = radar.nextLine();
+    		System.out.println("Please enter the number of tweets you would like displayed.");
+    		numTweets = radar.nextInt();
+    	
+    		System.out.println("Displaying " + numTweets + " of the most recent tweets about " + searchTerm + "...");
+    		System.out.print("\n");
+    	
+    		//Begins a new search of Twitter's website using the search term. 
+    		TwitterSearch nws = new TwitterSearch();
+    		ArrayList<Tweet> current = nws.query(searchTerm, "Recent", numTweets);
+    		//Displays the text of the Tweet and its author.
+    		for(int j = 0; j <= numTweets - 1; j++){
+    			System.out.println(current.get(j).getUser());
+    			System.out.println(current.get(j).getText());
+    			System.out.print("\n");
+    			}
+    		
+    		//Catch block for exceptions.
+    		}catch (NullPointerException excpt){
+    			System.out.print("Caught NullPointerException");
+    		}	
+    	}
+    }
---
+++ twitter-search/realtimeweb/twittersearch/Constants.java
@@ -0,0 +1,9 @@
+package realtimeweb.twittersearch;
+
+public final class Constants {
+	//Twitter Authentication 
+	public static final String consumerKey = "oIAT6iUZ4aU3go2fm4B4wyNg8" ;
+	public static final String consumerSecret = "KdKf2PHrak0nP8nAe5vMxQiKkiJKZ36izfDngKGVS08CJJk6Q7" ;
+	public static final String token = "886023686-zF7me6ggqULeKzZHyQ2e4RZKCoRgykFLczSvhyFN" ;
+	public static final String tokenSecret = "r5rBlBahdVXZwcDCqbgJwVAPW6jwbzw6brS3fUXiK3NDe" ;
+}
