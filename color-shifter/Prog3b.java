18879dcad3ca7c8e9433e21bb033509dcf7e4265
Joseph Tinglof <jbtinglof@gmail.com>
2018-11-17 11:50:42 -0800

    Update Prog3b.java

--- color-shifter/Prog3b.java
+++ color-shifter/Prog3b.java
@@ -7,9 +7,7 @@ import java.util.Scanner;
  *  @author  Joseph Tinglof
  */
 public class Prog3b {
-
 	public static void main(String[] args) {
-		System.out.println("Program 3, Joseph Tinglof, masc0752");
 		
 		Scanner scan = new Scanner(System.in);
 		ImageConverter ic = new ImageConverter();
